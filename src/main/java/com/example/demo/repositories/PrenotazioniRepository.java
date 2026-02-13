8ae0e891c73a0f0b66261c7658d0356b21ae9a4c
AntonioM. <antoniomeronew@gmail.com>
2026-02-13 11:17:38 +0100

    repo

---
+++ src/main/java/com/example/demo/repositories/PrenotazioniRepository.java
@@ -0,0 +1,14 @@
+package com.example.demo.repositories;
+
+
+import com.example.demo.entities.Prenotazione;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+
+
+@Repository
+public interface PrenotazioniRepository extends JpaRepository<Prenotazione, Long> {
+    boolean existsByDipendenteIdAndViaggioData(Long dipendenteId, LocalDate dataViaggio);
+}
